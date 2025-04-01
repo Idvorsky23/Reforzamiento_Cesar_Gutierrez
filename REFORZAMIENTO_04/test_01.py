227df3874246d8b7362f73c2bfd92b5b75a2844f
Cesar Gutierrez <idvorsky23@gmail.comgit config --global user.name Cesar Gutierrez>
2025-03-31 22:34:26 -0500

    reforzamiento 4

---
+++ REFORZAMIENTO_04/test_01.py
@@ -0,0 +1,5 @@
+tamaño = int(input("Ingrese el tamaño de la lista: "))
+nombres = list(map(lambda _: input("Ingrese un nombre: "), range(tamaño)))
+print("______________________________")
+print(f"Tamaño de la lista: {len(nombres)}")
+print("Nombres ingresados:", nombres)
---
+++ REFORZAMIENTO_04/test_02.py
@@ -0,0 +1,12 @@
+departamentos = ["Lima", "Cusco", "Arequipa", "Piura", "Trujillo"]
+print("Lista actual:", departamentos)
+
+dep1 = input("Ingrese el departamento a reemplazar: ")
+dep2 = input("Ingrese el nuevo departamento: ")
+
+if dep1 in departamentos:
+    index = departamentos.index(dep1)
+    departamentos[index] = dep2
+    print("Lista actualizada:", departamentos)
+else:
+    print("El departamento no está en la lista.")
